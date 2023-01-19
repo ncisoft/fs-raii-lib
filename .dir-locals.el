91da7cceeb34773319ec80979e9f4f5ad56d7463
Young Lee <ncisoft@hotmail.com>
2023-01-19 21:46:53 +0800

    feature: add cmake support

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+;;;    Note: you can easily add a directory local variable with SPC f v d.
+
+((c++-mode (c-c++-backend . lsp-ccls)))
